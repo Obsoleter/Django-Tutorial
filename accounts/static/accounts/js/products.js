292b6a730cfbf6325b486d4c809f08940d60ff1a
Ivan Chernih <chernih42@yandex.ru>
2021-09-17 09:04:23 +0300

    Improve Frontend Add functionality Make code OOP based

--- accounts/static/accounts/js/products.js
+++ accounts/static/accounts/js/products.js
@@ -1,11 +1,4 @@
 //Tools
-function getProductId() {
-    const url = new URL(window.location.href)
-    args = url.pathname.split('/', 3)
-    return args[args.length - 1]
-}
-
-
 function getCookie(name) {
     let cookieValue = null;
     if (document.cookie && document.cookie !== '') {
@@ -23,162 +16,301 @@ function getCookie(name) {
 }
 
 
-//Comments tools
-function insertComment(user, comment) {
-    element = document.querySelector('#insert_comments')
-    template = document.getElementById('comment_template').innerHTML
-    template = template
-    .replace('\{comment_id\}', comment['id'])
-    .replace('\{comment_username\}', comment['account'])
-    .replace('\{comment_desc\}', comment['comment'])
+// New OOP code
 
-    element.insertAdjacentHTML('beforebegin', template)
+class User {
+    constructor() { }
 
-    if (user['user'] == comment['account']) {
-        control_template = document.getElementById('comment_control_template').innerHTML
-        .replaceAll('\{comment_id\}', comment['id'])
+    async fetch(user) {
+        let user = await fetch('http://localhost/api/accounts/')
+        .then(response => {
+            if (!response.ok) {
+                throw Error('Couldn\'t fetch user!')
+            }
+            return response.json()
+        })
+        .catch(error => {
+            console.log(error)
+        })
 
-        element = document.querySelector('#comment' + comment['id'])
+        this.id = user['id']
+        this.name = user['user']
+    }
 
-        element.insertAdjacentHTML('beforeend', control_template)
+    logUser() {
+        console.log(this.id)
+        console.log(this.name)
+    }
+}
+
+class Product {
+    constructor(user) {
+        this.productId = this.getProductId()
+        this.user = user
+    }
+
+    async fetch() {
+        let product = await fetch('http://localhost/api/products/' + this.productId + '/')
+        .then(response => {
+            if (!response.ok) {
+                throw Error('Couldn\'t fetch comments!')
+            }
+            return response.json()
+        })
+        .catch(error => {
+            console.log(error)
+        })
+
+        this.name = product['name']
+        this.author = product['author']
+        this.desc = product['description']
+
+        this.insertProduct()
+    }
+
+    insertProduct() {
+        document.querySelector('#product_name').insertAdjacentHTML('beforeend', this.name)
+        document.querySelector('#product_author').insertAdjacentHTML('beforeend', this.author)
+        document.querySelector('#product_description').insertAdjacentHTML('beforeend', this.desc)
+
+        if (this.author == this.user.name) {
+            let element = document.getElementById('product' + this.productId)
+            let template = document.getElementById('product_control_template').innerHTML
+            
+            element.insertAdjacentHTML('beforeend', template)
+        }
+    }
+
+    getProductId() {
+        if(this.productId) {
+            return this.productId
+        }
+
+        const url = new URL(window.location.href)
+        const args = url.pathname.split('/', 3)
+        return args[args.length - 1]
+    }
+
+    delete() {
+        const token = document.querySelector('input[name="csrfmiddlewaretoken"]').getAttribute('value')
+
+        fetch('http://localhost/api/products/' + this.productId, {
+            credentials: 'include',
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CSRFToken': token
+            }
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw Error(`Couldn\'t delete product with id: ${this.productId}\n` + response.statusText)
+            }
+            const redirect = document.querySelector('#product_delete_redirect').value
+            document.location.href = redirect
+        })
+    }
+}
+
+class Comments {
+    constructor(user, productId) {
+        this.user = user
+        this.productId = productId
+    }
+
+    async fetch() {
+        const json = await fetch('http://localhost/api/comments/?product=' + this.productId)
+        .then(response => {
+            if (!response.ok) {
+                throw Error('Couldn\'t fetch product!')
+            }
+            return response.json()
+        })
+        .catch(error => {
+            console.log(error)
+        })
+
+        this.comments = json.results
+        for (const comment of this.comments) {
+            this.insertComment(comment)
+        }
+    }
+
+    insertComment(comment) {
+        let element = document.querySelector('#insert_comments')
+        let template = document.getElementById('comment_template').innerHTML
+        template = template
+        .replaceAll('\{comment_id\}', comment.id)
+        .replaceAll('\{comment_username\}', comment.account)
+        .replaceAll('\{comment_desc\}', comment.comment)
+
+        element.insertAdjacentHTML('beforebegin', template)
+
+        document.getElementById('comment_text' + comment.id).style.display = 'none'
+
+        if (this.user.name == comment.account) {
+            let control_template = document.getElementById('comment_control_template').innerHTML
+            .replaceAll('\{comment_id\}', comment.id)
+
+            element = document.querySelector('#comment' + comment.id)
+
+            element.insertAdjacentHTML('beforeend', control_template)
+
+            document.getElementById('comment_cancel' + comment.id).style.display = 'none'
+            document.getElementById('comment_apply' + comment.id).style.display = 'none'
+        }
+    }
+
+    delete(commentId) {
+        const token = document.querySelector('input[name="csrfmiddlewaretoken"]').getAttribute('value')
+
+        fetch('http://localhost/api/comments/' + commentId, {
+            credentials: 'include',
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CSRFToken': token
+            }
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw Error(`Couldn\'t delete comment with id: ${commentId}\n` + response.statusText)
+            }
+            document.querySelector('#comment' + commentId).remove()
+        })
+    }
+
+    create() {
+        const text = document.querySelector('#id_comment').value
+    
+        if (!text.length) {
+            return
+        }
+    
+        const token = document.querySelector('input[name="csrfmiddlewaretoken"]').getAttribute('value')
+    
+        const comment = {
+            'product': 'http://localhost/api/products/' + this.productId + '/',
+            'comment': text
+        }
+    
+        fetch('http://localhost/api/comments/', {
+            credentials: 'include',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CSRFToken': token
+            },
+            body: JSON.stringify(comment)
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw Error('Couldn\'t POST comment!\n' + response.statusText)
+            }
+            return response.json()
+        })
+        .then(json => {
+            this.insertComment(json)
+            document.querySelector('#id_comment').value = ''
+        })
+        .catch(error => {
+            console.log(error)
+        })
+    }
+
+    edit(commentId) {
+        //Change HTML View
+        document.getElementById('comment_text' + commentId).style.display = ''
+        document.getElementById('comment_delete' + commentId).style.display = 'none'
+        document.getElementById('comment_update' + commentId).style.display = 'none'
+        document.getElementById('comment_cancel' + commentId).style.display = ''
+        document.getElementById('comment_apply' + commentId).style.display = ''
+        document.getElementById('comment_desc' + commentId).style.display = 'none'
+        //Put text before
+        let commentIndex = this.findComment(commentId)
+        let comment = this.comments[commentIndex]
+        document.getElementById('comment_text' + commentId).value = comment.comment
+    }
+
+    cancelEdit(commentId) {
+        //Set previous Text
+        let commentIndex = this.findComment(commentId)
+        let comment = this.comments[commentIndex]
+        document.getElementById('comment_desc' + commentId).innerHTML = comment.comment
+        //Change HTML View Back
+        document.getElementById('comment_text' + commentId).style.display = 'none'
+        document.getElementById('comment_delete' + commentId).style.display = ''
+        document.getElementById('comment_update' + commentId).style.display = ''
+        document.getElementById('comment_cancel' + commentId).style.display = 'none'
+        document.getElementById('comment_apply' + commentId).style.display = 'none'
+        document.getElementById('comment_desc' + commentId).style.display = ''
+    }
+
+    update(commentId) {
+        //Get New Text
+        let text = document.getElementById('comment_text' + commentId).value
+        if (!text.length) {
+            return
+        }
+        //Update Comment (query and this.comments)
+        const token = document.querySelector('input[name="csrfmiddlewaretoken"]').getAttribute('value')
+    
+        const comment = {
+            'comment': text,
+            'product': 'http://localhost/api/products/' + this.productId + '/'
+        }
+    
+        fetch('http://localhost/api/comments/' + commentId + '/', {
+            credentials: 'include',
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CSRFToken': token
+            },
+            body: JSON.stringify(comment)
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw Error('Couldn\'t POST comment!\n' + response.statusText)
+            }
+            return response.json()
+        })
+        .then(json => {
+            document.querySelector('#id_comment').value = ''
+
+            let commentIndex = this.findComment(commentId)
+            this.comments[commentIndex].comment = text
+            this.cancelEdit(commentId)
+        })
+        .catch(error => {
+            console.log(error)
+        })
+    }
+
+    findComment(commentId) {
+        let index = 0
+
+        for (let comment of this.comments) {
+            if (comment.id == commentId) {
+                return index
+            }
+            index += 1
+        }
+        return undefined
     }
 }
 
 
-function deleteComment(commentId) {
-    token = document.querySelector('input[name="csrfmiddlewaretoken"]').getAttribute('value')
+var user = new User()
+var product = new Product(user)
+var comments = new Comments(user, product.getProductId())
 
-    fetch('http://localhost/api/comments/' + commentId, {
-        credentials: 'include',
-        method: 'DELETE',
-        headers: {
-            'Content-Type': 'application/json',
-            'X-CSRFToken': token
-        }
-    })
-    .then(response => {
-        if (!response.ok) {
-            throw Error(`Couldn\'t delete comment with id: ${commentId}\n` + response.statusText)
-        }
-        document.querySelector('#comment' + commentId).remove()
-    })
+async function loadPage() {
+    await user.fetch()
+
+    product.fetch()
+
+    comments.fetch()
 }
 
-
-function createComment1() {
-    text = document.querySelector('#id_comment').value
-
-    if (text.length == 0 || gUser == undefined) {
-        return
-    }
-
-    token = document.querySelector('input[name="csrfmiddlewaretoken"]').getAttribute('value')
-
-    comment = {
-        'product': 'http://localhost/api/products/' + getProductId() + '/',
-        'comment': text
-    }
-
-    fetch('http://localhost/api/comments/', {
-        credentials: 'include',
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'X-CSRFToken': token
-        },
-        body: JSON.stringify(comment)
-    })
-    .then(response => {
-        if (!response.ok) {
-            throw Error('Couldn\'t POST comment!\n' + response.statusText)
-        }
-        return response.json()
-    })
-    .then(json => {
-        insertComment(gUser, json)
-        document.querySelector('#id_comment').value = ''
-    })
-    .catch(error => {
-        console.log(error)
-    })
-}
-
-
-//Product tools
-function insertProduct(user, product) {
-    document.querySelector('#product_name').insertAdjacentHTML('beforeend', product['name'])
-    document.querySelector('#product_author').insertAdjacentHTML('beforeend', product['author'])
-    document.querySelector('#product_description').insertAdjacentHTML('beforeend', product['description'])
-}
-
-
-//Fetching
-//User
-function fetchUser() {
-    fetch('http://localhost/api/accounts/')
-    .then(response => {
-        if (!response.ok) {
-            throw Error('Couldn\'t fetch user!')
-        }
-        return response.json()
-    })
-    .then(json => {
-        userFetched(json)
-    })
-    .catch(error => {
-        console.log(error)
-    })
-}
-
-
-//Comments
-function fetchComments(user, productId) {
-    fetch('http://localhost/api/comments/?product=' + productId)
-    .then(response => {
-        if (!response.ok) {
-            throw Error('Couldn\'t fetch product!')
-        }
-        return response.json()
-    })
-    .then(json => {
-        for (comment of json.results) {
-            insertComment(user, comment)
-        }
-    })
-    .catch(error => {
-        console.log(error)
-    })
-}
-
-
-//Product
-function fetchProduct(user, productId) {
-    fetch('http://localhost/api/products/' + productId + '/')
-    .then(response => {
-        if (!response.ok) {
-            throw Error('Couldn\'t fetch comments!')
-        }
-        return response.json()
-    })
-    .then(json => {
-        insertProduct(user, json)
-    })
-    .catch(error => {
-        console.log(error)
-    })
-}
-
-
-//Fetch stuff & Execution
-function userFetched(user) {
-    gUser = user
-
-    productId = getProductId()
-
-    fetchComments(user, productId)
-    fetchProduct(user, productId)
-}
-
-
-var gUser = undefined
-fetchUser()
+loadPage()
