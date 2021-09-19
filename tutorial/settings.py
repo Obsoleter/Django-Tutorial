0a5c000eb3241ddce3dfc4065f5230321033e3ad
Ivan Chernih <chernih42@yandex.ru>
2021-09-19 07:26:40 +0300

    Migrate to MySQL

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'tutorial.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'djangotutorial',
+        'USER': 'djangotutorial',
+        'PASSWORD': 'django-tutorial',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
